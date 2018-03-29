6feea4abd902c32726f66442a2a34b539aa5fc07
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2018-03-29 07:22:00 -0300

    Create Footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+
+const Footer = () => (
+  <div className="Footer">
+    <p>Matheus Cruz Rocha</p>
+  </div>
+);
+
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './app';
 export { default as DataTable } from './data-table';
+export { default as Footer } from './footer';
 export { default as Header } from './header';
 export { default as SubHeader } from './sub-header';

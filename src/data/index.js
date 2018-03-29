fbff7001c9be5e1ac882916b3b5753dca7ac2754
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2018-03-29 07:33:18 -0300

    Fix the repeated id on data

--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,5 @@ export default [
   { id: 1, profissao: 'medicina', instituto: 'Osvaldo Cruz' },
   { id: 2, 'profissao': 'fisioterapia', instituto: 'Sarah Kubtschek' },
   { id: 3, profissao: 'odontologia', instituto: 'Hospital São Paulo' },
-  { id: 3, profissao: 'enfermagem', instituto: 'Hospital Albert Einstein' }
+  { id: 4, profissao: 'enfermagem', instituto: 'Hospital Albert Einstein' }
 ];

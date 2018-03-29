4c86adf0b070269337a1cc378902b28026a28125
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2018-03-28 22:29:10 -0300

    Create the Initial Data of the project

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, profissao: 'medicina', instituto: 'Osvaldo Cruz' },
+  { id: 2, 'profissao': 'fisioterapia', instituto: 'Sarah Kubtschek' },
+  { id: 3, profissao: 'odontologia', instituto: 'Hospital São Paulo' },
+  { id: 3, profissao: 'enfermagem', instituto: 'Hospital Albert Einstein' }
+];

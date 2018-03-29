36e9564c689f437cf6658fd4277ea6a94140853e
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2018-03-29 10:16:21 -0300

    Using form modal component and improving CSS

--- src/components/sub-header/index.js
+++ src/components/sub-header/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
+import { AddExamFormModal } from '../';
 import './styles.css';
 
 
-const SubHeader = () => (
-  <div className="SubHeader clearfix">
-    <div className="SubHeader-title-wrapper">
-      <h1>Provas</h1>
-    </div>
-    <div className="SubHeader-add-button-wrapper">
-      <button>Novo</button>
-    </div>
-  </div>
-);
+class SubHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  handleClose = () => this.setState({ showModal: false });
+  handleShow = () => this.setState({ showModal: true });
+
+  render() {
+    const { addNewExam } = this.props;
+
+    return (
+      <div className="SubHeader clearfix">
+        <div className="SubHeader-title-wrapper">
+          <h4>Provas</h4>
+        </div>
+        <div className="SubHeader-add-button-wrapper">
+          <Button bsStyle="success" onClick={this.handleShow}>Novo</Button>
+        </div>
+        <AddExamFormModal
+          show={this.state.showModal}
+          addNewExam={addNewExam}
+          handleClose={this.handleClose}
+        />
+      </div>
+    );
+  }
+}
 
 
 export default SubHeader;

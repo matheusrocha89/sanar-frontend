ad9c491bafc62270a1f4f0164b76b349634ceefa
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2018-03-29 10:16:51 -0300

    Using data as state of the app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
 import { Header, DataTable, SubHeader, Footer } from '../';
+import Data from '../../data';
 import './styles.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: Data,
+    };
+  }
+
+  addNewExam = (exam) => this.setState({
+    data: [...this.state.data, exam],
+  });
+
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="container">
-          <SubHeader />
-          <DataTable />
+        <div className="App-container container">
+          <SubHeader addNewExam={this.addNewExam} />
+          <DataTable data={this.state.data} />
         </div>
         <Footer />
       </div>
